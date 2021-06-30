e01a60a2e900668b2803071b89e55cc2ab23222c
ArnabXD <arnabxd@pm.me>
2021-06-30 21:06:06 +0530

    160K seems smoother

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process';
 
-const args = "-c:a libopus -b:a 192K -acodec pcm_s16le -f s16le -ac 1 -ar 48000 pipe:1"
+const args = "-c:a libopus -b:a 160K -acodec pcm_s16le -f s16le -ac 1 -ar 48000 pipe:1"
 
 export const ffmpeg = (input: string) => {
     return spawn("ffmpeg", ["-y", "-nostdin", "-i", `${input}`, ...args.split(' ')]).stdout
