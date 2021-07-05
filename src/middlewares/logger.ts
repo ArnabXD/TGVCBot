dbfb1c8bbfc499d9ff6b1fb7a8b37b2bb9df661b
ArnabXD <arnabxd@pm.me>
2021-07-05 12:56:19 +0530

    show error

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,10 +2,11 @@ import { Context, MiddlewareFn } from 'telegraf';
 import { log } from '../bot';
 import { escape } from 'html-escaper';
 
-export const Logger: MiddlewareFn<Context> = async (_, next) => {
+export const Logger: MiddlewareFn<Context> = async (ctx, next) => {
     try {
         await next();
     } catch (err) {
+        await ctx.replyWithHTML(`<b>Error :</b>\n <code>${escape(String(err))}</code>`);
         await log(`<b>Error :</b>\n <code>${escape(String(err))}</code>`);
     }
 }
