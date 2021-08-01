4bcd1ec54bda9823ac9e127ffc42dde441f9d192
ArnabXD <arnabxd@pm.me>
2021-08-01 15:25:04 +0530

    Initialize Middlewares (#deploy)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import bot, { log } from './bot';
 import { startUserBot } from './userbot';
+import { InitMiddleWares } from './middlewares';
 import { InitHandlers } from './handlers';
 
 (async () => {
+    InitMiddleWares();
     InitHandlers();
     await bot.launch({
         dropPendingUpdates: true,
