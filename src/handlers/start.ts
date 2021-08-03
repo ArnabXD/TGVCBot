2b100f856970eda7b4ea9beb4e0936180d1d97f8
ArnabXD <arnabxd@pm.me>
2021-08-03 10:13:38 +0530

    fix: sending first_name twice | #deploy

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,7 +2,7 @@ import { Composer, Markup } from 'telegraf';
 
 export const Start = Composer.command('start', async (ctx) => {
     let text =
-        `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.first_name}</a>\n` +
+        `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.last_name}</a>\n` +
         `I Play Songs in Group Voice Chats.\n` +
         `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`
     await ctx.replyWithHTML(text, {
