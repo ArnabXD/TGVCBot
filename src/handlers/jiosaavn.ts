21ff2088d266cfadfd38c08406fe8ba03df35eb7
ArnabXD <arnabxd@pm.me>
2022-03-19 16:24:44 +0530

    refactor: stream provider and jiosaavn handler

--- src/handlers/jiosaavn.ts
+++ src/handlers/jiosaavn.ts
@@ -7,117 +7,68 @@
  */
 
 import { Composer, InlineKeyboard } from 'grammy';
-import axios from 'axios';
-import { escape } from 'html-escaper';
-import { stringify } from 'querystring';
 import { commandExtractor } from '../utils';
-import {
-  JioSaavnSongResponse,
-  JioSaavnSongSearchResponse
-} from '../types/jiosaavn.response';
+import { jiosaavn } from '../providers/jiosaavn';
 import { tgcalls } from '../tgcalls';
+import { escape } from 'html-escaper';
 
 const composer = new Composer();
 
-composer.use(async (ctx, next) => {
+composer.command(['jiosaavn', 'jsvn'], async (ctx) => {
+  await ctx.api.sendChatAction(ctx.chat.id, 'typing');
   if (ctx?.chat?.type === 'private') {
     return await ctx.reply('This Command works on Group Only');
   }
-  await next();
-});
-
-composer.command(['jiosaavn', 'jsvn'], async (ctx) => {
-  await ctx.api.sendChatAction(ctx.chat.id, 'record_voice');
-
   const { args: keyword } = commandExtractor(ctx.message?.text || '');
   if (!keyword) {
     await ctx.reply('Please Provide Search Keyword');
     return;
   }
-
-  const query = stringify({ query: keyword.replace(/\s/g, '+') });
-  const resp = (
-    await axios.get<JioSaavnSongSearchResponse[]>(
-      `https://jiosaavn-api.vercel.app/search?${query}`
-    )
-  ).data;
-
-  if (!resp[0]) {
+  const result = await jiosaavn.search(keyword);
+  if (!result) {
     await ctx.reply('No Results Found');
     return;
   }
-
-  const [result] = resp;
-  const song = (
-    await axios.get<JioSaavnSongResponse>(
-      `https://jiosaavn-api.vercel.app/song?id=${result.id}`
-    )
-  ).data;
-
   if ('title' in ctx.chat && ctx.from) {
+    const songData = await jiosaavn.getSong(result[0].id, ctx.from);
     await tgcalls.streamOrQueue(
       { id: ctx.chat.id, name: ctx.chat.title },
-      {
-        link: song.perma_url,
-        title: song.song.replace(/&quot;/g, `"`),
-        image: song.image,
-        artist: song.singers || song.primary_artists,
-        duration: song.duration,
-        requestedBy: {
-          id: ctx.from.id,
-          first_name: ctx.from.first_name
-        },
-        mp3_link: song.other_qualities.pop()?.url ?? song.media_url,
-        provider: 'jiosaavn'
-      }
+      songData
     );
   }
 });
 
 composer.command(['jiosaavnsearch', 'jsvnsearch', 'jsvnsr'], async (ctx) => {
   await ctx.api.sendChatAction(ctx.chat.id, 'typing');
-
-  const { args: keyword } = commandExtractor(ctx.message?.text || '');
-  if (!keyword) {
+  if (ctx?.chat?.type === 'private') {
+    return await ctx.reply('This Command works on Group Only');
+  }
+  const { args } = commandExtractor(ctx.message?.text || '');
+  if (!args) {
     await ctx.reply('Please Provide Search Keyword');
     return;
   }
-
-  const query = stringify({ query: keyword.replace(/\s/g, '+') });
-  const resp = (
-    await axios.get<JioSaavnSongSearchResponse[]>(
-      `https://jiosaavn-api.vercel.app/search?${query}`
-    )
-  ).data;
-
-  if (!resp[0]) {
+  let result = await jiosaavn.search(args);
+  if (!result) {
     await ctx.reply('No Results Found');
     return;
   }
-
-  let text = `Search Results for <b>${keyword}</b>\n\n`;
+  result = result.slice(0, 10);
+  let text = `Search Results for <b>${args}</b>\n\n`;
   const keyboard = new InlineKeyboard();
-
-  resp.slice(0, 10).forEach((data, index) => {
-    index += 1;
+  result.forEach((res, index) => {
+    index++;
     text +=
-      '<b>' +
-      index +
-      ':</b> ' +
-      '<a href="' +
-      data.url +
-      '">' +
-      escape(data.title.replace(/&quot;/g, `"`)) +
-      '</a>\n';
-    keyboard.text(`${index}`, 'jsvn:' + ctx.from?.id + ':' + data.id);
+      `${index} <b><a href="${res.perma_url}">${escape(res.title)}</a></b>\n` +
+      `<b>Artist :</b> ${escape(res.more_info.singers || '')}\n\n`;
+    keyboard.text(`${index}`, 'jsvn:' + ctx.from?.id + ':' + res.id);
     if (!(index % 5)) {
       keyboard.row();
     }
   });
-
-  return await ctx.reply(text, {
-    reply_markup: keyboard,
+  await ctx.reply(text, {
     parse_mode: 'HTML',
+    reply_markup: keyboard,
     disable_web_page_preview: true
   });
 });
@@ -127,42 +78,22 @@ composer.callbackQuery(/^jsvn:\d+:[a-zA-Z0-9.\-_]/, async (ctx) => {
   const supportedUser = parseInt(query[1], 10);
   const songId = query[2];
   const clickedBy = ctx.callbackQuery.from.id;
-
   if (clickedBy !== supportedUser) {
     return await ctx.answerCallbackQuery({
       text: "You aren't allowed",
       show_alert: true
     });
   }
-
-  const song = (
-    await axios.get<JioSaavnSongResponse>(
-      'https://jiosaavn-api.vercel.app/song?id=' + songId
-    )
-  ).data;
-
   if (ctx.chat && 'title' in ctx.chat && ctx.from) {
+    const songData = await jiosaavn.getSong(songId, ctx.from);
     await tgcalls.streamOrQueue(
       {
         id: ctx.chat.id,
         name: ctx.chat.title
       },
-      {
-        link: song.perma_url,
-        title: song.song.replace(/&quot;/g, `"`),
-        image: song.image,
-        artist: song.singers,
-        duration: song.duration,
-        requestedBy: {
-          id: ctx.from.id,
-          first_name: ctx.from.first_name
-        },
-        mp3_link: song.other_qualities.pop()?.url ?? song.media_url,
-        provider: 'jiosaavn'
-      }
+      songData
     );
   }
-
   await ctx.deleteMessage();
 });
 
