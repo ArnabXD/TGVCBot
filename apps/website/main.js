bdab7b84725fa083b28fa0d03c67354a49a51ebc
ArnabXD <arnabxd@pm.me>
2022-04-29 19:12:33 +0530

    feat: vite to remix

--- apps/website/main.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-undef */
-import 'virtual:windi.css';
-import './style.css';
--- apps/website/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-import WindiCSS from 'vite-plugin-windicss';
-
-export default defineConfig({
-  plugins: [WindiCSS()]
-});
