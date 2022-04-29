8025a14c086ef11e60bd8008a7d6b825706d14d2
ArnabXD <arnabxd@pm.me>
2022-04-29 19:30:04 +0530

    fix: upgrade not working on linux script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,11 @@
 wget https://github.com/ArnabXD/TGVCBot/releases/latest/download/archive.tgz
 tar xf archive.tgz
 rm archive.tgz
-mv -f package tgvcbot
+if [-d "tgvcbot"]
+then
+  cp -r package/* tgvcbot/
+else
+  mv -f package tgvcbot
 
 cd tgvcbot
 npm install --production
