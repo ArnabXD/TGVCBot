86b570eec6598e5501be51689116eea9d8230a23
ArnabXD <arnabxd@pm.me>
2022-04-30 18:13:03 +0530

    feat: tailwind and mdx

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [require('remark-gfm')],
+    rehypePlugins: []
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  }
+});
+module.exports = withMDX({
+  // Append the default value with md extensions
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+});
