3dddd0b9d25556a4e9bae17bcda2e01e09f4ad2c
ArnabXD <arnabxd@pm.me>
2022-04-30 19:02:27 +0530

    chore: update gradient

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -3,11 +3,13 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="bg-gradient-to-br from-rose-700 to-pink-600 text-white">
+      <div className="bg-gradient-to-br from-sky-400 to-cyan-300 text-white">
         <div className="flex flex-1 items-center mx-auto max-w-screen-xl px-3 py-12 md:p-6 min-h-[20vh] md:min-h-[50vh]">
           <div className="">
-            <h1 className="text-5xl md:text-6xl font-semibold">TGVCBot</h1>
-            <p className="text-xl my-1 font-medium">
+            <h1 className="text-5xl md:text-6xl font-semibold drop-shadow-lg">
+              TGVCBot
+            </h1>
+            <p className="text-xl my-1 font-medium drop-shadow-md">
               Stream Music on Telegram Video Chat
             </p>
           </div>
