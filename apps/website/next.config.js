9fda2c0a764e9374411d3e55365f010e0023ac34
ArnabXD <arnabxd@proton.me>
2022-05-10 12:10:04 +0530

    chore(deps): remove remark-gfm

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,7 +1,7 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [require('remark-gfm')],
+    remarkPlugins: [],
     rehypePlugins: []
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
